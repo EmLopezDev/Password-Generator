c44fbe05f7dd9362aae01ee84add3b832cf70776
Emmanuel Lopez <emlopezdev@gmail.com>
2025-05-13 14:17:42 -0400

    stored all checked boxes checked values

--- js/main.js
+++ js/main.js
@@ -11,8 +11,12 @@ const strengthGauges = document.querySelectorAll(".card__body--strength-gauge");
 
 const generateButton = document.getElementById("generate-button");
 
+let lengthValue;
+const checkedOptions = {};
+
 lengthRange.addEventListener("input", (e) => {
     const rangeValue = e.target.value;
+    lengthValue = rangeValue;
     lengthNumber.textContent = rangeValue;
     // Calculate percentage of range and turns it into a whole number
     const wholeNumber = (rangeValue / lengthRange.max) * 100;
@@ -20,4 +24,15 @@ lengthRange.addEventListener("input", (e) => {
     const progress = wholeNumber.toFixed(0);
 
     lengthRange.style.background = `linear-gradient(to right, var(--color-green-200) ${progress}%, var(--color-grey-850) ${progress}%)`;
+});
+
+const isChecked = () => {
+    checkboxes.forEach((cb) => {
+        checkedOptions[`${cb.value}`] = cb.checked;
+    });
+    console.log(lengthValue);
+};
+
+checkboxes.forEach((cb) => {
+    cb.addEventListener("input", isChecked);
 });
